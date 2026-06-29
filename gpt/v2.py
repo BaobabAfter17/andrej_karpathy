9f6df2dfc93eae7c011b4d604824c78a67d56cb4
Yakun Yao <yakunyao@gmail.com>
2026-06-29 10:30:05 +0800

    gpt: CRUX of self-attention; apply one head self-attention to BigramLanguageModel

--- gpt/v2.py
+++ gpt/v2.py
@@ -5,9 +5,9 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32
 block_size = 8
-max_iters = 3000
-eval_interval = 300
-learning_rate = 1e-2
+max_iters = 5000
+eval_interval = 500
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 32
@@ -57,6 +57,29 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+    """ one head of self-attention"""
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+    
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        # compute attention scores - affinities
+        wei = q @ k.transpose(-2, -1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))    # (B, T, T)
+        wei = F.softmax(wei, dim=-1)    # (B, T, T)
+        # perform the weighted aggregation of the values
+        v = self.value(x)
+        out = wei @ v   # (B, T, T) @ (B, T, C) -> (B, T, C)
+        return out
+
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
@@ -64,6 +87,7 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_head = Head(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
     
     def forward(self, idx, targets=None):
@@ -73,6 +97,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx)    # (B, T, C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))    # (T, C)
         x = tok_emb + pos_emb   # (B, T, C)
+        x = self.sa_head(x)     # apply on head of self-attention
         logits = self.lm_head(x)    # (B, T, vocab_size)
 
         if targets is None:
